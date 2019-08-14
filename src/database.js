23d5d8489dfc7663fe5e5a02b2a05fe4049fc0b1
brayan <brayanhernandezmartinez@gmail.com>
2019-08-14 13:07:54 -0500

    Primera parte back

---
+++ src/database.js
@@ -0,0 +1,10 @@
+//Archivo para conectar a una base de datos
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/notes-db-app', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.error(err));
